9690efeaf5aeea9149cb3a6d196b43f9bd609a38
NathanKoehler <flak.ultra@gmail.com>
2018-06-12 09:59:41 -0400

    Initial commit

---
+++ Source/IHateYouSheriff/IHateYouSheriffGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IHateYouSheriffGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AIHateYouSheriffGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AIHateYouSheriffGameMode();
+};
+
+
+
